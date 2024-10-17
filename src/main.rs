c36b5a7f9c8b85fe72628086faaf8f27f412e2a7
Fernando Perry <fernandoperry1234@gmail.com>
2024-10-17 20:21:17 +0700

    added args support

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,55 @@ mod search;
 use axum::{routing::get, Router};
 use search::*;
 use serve::*;
+use std::env;
+
+struct ProgArgs {
+    port: String,
+    sql_path: String
+}
+
+impl ProgArgs {
+    fn default_value() -> ProgArgs {
+        return ProgArgs { port: "8081".to_string(), sql_path: "./db.sqlite".to_string() };
+    }
+}
+
+fn get_args() -> Vec<String> {
+    let ret: Vec<String> = env::args().collect();
+    return ret;
+}
+
+fn parse_args(args: Vec<String>) -> Option<ProgArgs> {
+    let mut res: ProgArgs = ProgArgs::default_value();
+    let mut idx = 1;
+    while idx < args.len() {
+        let current_arg = &args[idx];
+        match &current_arg[..] {
+            "-p" | "--port" => {
+                if idx + 1 <= args.len() -1 {
+                    res.port = args[idx+1].clone();
+                }
+                idx += 1;
+            }
+            "-d" | "--databse" => {
+                if idx + 1 <= args.len() -1 {
+                    res.sql_path = args[idx+1].clone();
+                }
+                idx += 1;
+            }
+            _ => {}
+        }
+        idx += 1;
+    }
+    return Some(res);
+}
 
 #[tokio::main]
 async fn main() {
+    let args = get_args();
+    let parsed: ProgArgs = parse_args(args).unwrap_or(ProgArgs::default_value());
     let ip: &str = "0.0.0.0";
-    let port: &str = "8000";
+    let port: &str = &parsed.port;
 
     let combine: &str = &format!("{}:{}", ip, port);
 
