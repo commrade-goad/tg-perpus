e2c3774bf802242d4d099589847987ec2a6d6cc5
Fernando Perry <fernandoperry1234@gmail.com>
2024-10-17 19:36:36 +0700

    added all the api (not implemented)

--- src/serve.rs
+++ src/serve.rs
@@ -3,30 +3,80 @@ use crate::search::*;
 use serde_derive::Deserialize;
 use axum::{extract::Query, response::IntoResponse, Json};
 
+// `/get_tag?s={startat}&e={endat}`
 #[derive(Deserialize)]
 pub struct GetTagParams {
     s: i32,
     e: i32,
 }
-// Handler for `/get_tag?s={startat}&e={endat}`
 pub async fn get_tag(Query(params): Query<GetTagParams>) -> impl IntoResponse {
-    let books = sample_books();
-    let mut tags: Vec<String> = books.iter().flat_map(|book| book.tags.clone()).collect();
-    tags.sort();
-    tags.dedup();
-
-    Json(tags)
+    Json("NOT IMPLEMENTED YET")
 }
 
-// Query parameters for `/search?q={query}`
+// `/search?q={query}`
 #[derive(Deserialize)]
 pub struct SearchParams {
     q: String,
 }
-
-// Handler for `/search?q={query}`
 pub async fn search_book(Query(params): Query<SearchParams>) -> impl IntoResponse {
     let res = s_search_book(&params.q);
     Json(res)
 }
 
+// `/get_book_info?id={id}`
+#[derive(Deserialize)]
+pub struct BookInfoParams{
+    id: String,
+}
+pub async fn get_book_info(Query(params): Query<BookInfoParams>) -> impl IntoResponse {
+    Json("NOT IMPLEMENTED YET")
+}
+
+// `/get_book_with_tags?t={tag}`
+#[derive(Deserialize)]
+pub struct GetBookListFromTagParams{
+    t: String,
+}
+pub async fn get_book_from_tag(Query(params): Query<GetBookListFromTagParams>) -> impl IntoResponse {
+    Json("NOT IMPLEMENTED YET")
+}
+
+// `/add_book?t={title}&a={author}&tg={tag} {tag}&im={image blob}`
+#[derive(Deserialize)]
+pub struct AddBookParams{
+    t: String,
+    a: String,
+    tg: String,
+    im: String,
+}
+pub async fn add_new_book(Query(params): Query<AddBookParams>) -> impl IntoResponse {
+    Json("NOT IMPLEMENTED YET")
+}
+
+// `/add_tag?n={name}&im={image blob}`
+#[derive(Deserialize)]
+pub struct AddTagParams{
+    n: String,
+    im: String,
+}
+pub async fn add_new_tag(Query(params): Query<AddTagParams>) -> impl IntoResponse {
+    Json("NOT IMPLEMENTED YET")
+}
+
+// `/del_book?id={book_id}`
+#[derive(Deserialize)]
+pub struct DelBookParams{
+    id: String,
+}
+pub async fn del_new_book(Query(params): Query<DelBookParams>) -> impl IntoResponse {
+    Json("NOT IMPLEMENTED YET")
+}
+
+// `/del_tag?id={tag_id}`
+#[derive(Deserialize)]
+pub struct DelTagParams{
+    id: String,
+}
+pub async fn del_new_tag(Query(params): Query<DelTagParams>) -> impl IntoResponse {
+    Json("NOT IMPLEMENTED YET")
+}
